22ff178bac85b6621483e1a639be7906d9b33216
eimaam <imamddahir@gmail.com>
2025-05-09 10:58:15 +0100

    feat: Add ESLint, PostCSS, Tailwind CSS configuration, and utility functions for class merging

---
+++ src/theme/antdTheme.ts
@@ -0,0 +1,45 @@
+export const lightTheme = {
+  token: {
+    colorPrimary: '#445c97',
+    colorSuccess: '#28a67c',
+    colorWarning: '#f5920a',
+    colorError: '#ff3b3b',
+    colorInfo: '#008ff5',
+    colorTextBase: '#343a40',
+    colorBgBase: '#ffffff',
+    borderRadius: 4,
+    wireframe: false,
+  },
+  components: {
+    Button: {
+      colorPrimary: '#445c97',
+      algorithm: true,
+    },
+    Input: {
+      colorPrimary: '#445c97',
+    },
+  },
+};
+
+export const darkTheme = {
+  token: {
+    colorPrimary: '#6979ac',
+    colorSuccess: '#4bc395',
+    colorWarning: '#fbae1f',
+    colorError: '#ff6464',
+    colorInfo: '#2aaeff',
+    colorTextBase: '#e9ecef',
+    colorBgBase: '#212529',
+    borderRadius: 4,
+    wireframe: false,
+  },
+  components: {
+    Button: {
+      colorPrimary: '#6979ac',
+      algorithm: true,
+    },
+    Input: {
+      colorPrimary: '#6979ac',
+    },
+  },
+};
